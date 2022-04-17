b595b81b1229d4359fbce15e206f68bb889dee4e
Kaisher Yezdany <kasher11yezdany@gmail.com>
2022-04-17 20:11:15 +0530

    first commit

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC0W-gdtc-R_zYkBBeMv6Ngdgpxg9f0jUQ',
+  authDomain: 'ecom2-fd920.firebaseapp.com',
+  databaseURL: 'https://ecom2-fd920-default-rtdb.firebaseio.com',
+  projectId: 'ecom2-fd920',
+  storageBucket: 'ecom2-fd920.appspot.com',
+  messagingSenderId: '1099244302366',
+  appId: '1:1099244302366:web:a3adf40350a3a78cd83e0f'
+};
+
+export default firebaseConfig;
